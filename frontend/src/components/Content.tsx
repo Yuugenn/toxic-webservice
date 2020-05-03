ee23a0800c00a073e0b7fcd867dae54c46694ff1
Matthias Nunn <matthiasnunn@gmx.de>
2020-05-03 23:08:13 +0200

    changes frontend

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -3,29 +3,34 @@ import {Route, Switch} from "react-router-dom";
 import {Theme} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
 import Home from './Home';
+import Login from './Login';
 
 
 const useStyles = makeStyles((theme: Theme) => ({
+
 	content: {
 		height: '100%'
 	}
 }));
 
+
 function Content () {
+
 	const classes = useStyles();
 
 	return (
 		<div className={classes.content}>
 			<Switch>
-				<Route path="/upload">
-					Upload
-				</Route>
-				<Route path="/">
+			    <Route exact path={["/", "/login"]}>
+                    <Login/>
+                </Route>
+				<Route exact path="/home">
 					<Home/>
 				</Route>
 			</Switch>
 		</div>
 	);
 }
+
 
 export default Content;
