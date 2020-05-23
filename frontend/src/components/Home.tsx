9866f72f3cebe85f43ba8be72073cc5aecac9101
Matthias Nunn <matthiasnunn@gmx.de>
2020-05-23 20:56:53 +0200

    change home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,9 @@
 import {makeStyles} from '@material-ui/core';
-import {Button, Paper, SvgIcon, TextField} from "@material-ui/core";
-import React, {useState} from "react";
+import {Button, CircularProgress, Paper, SvgIcon, Table, TableBody, TableCell, TableRow, TextField, Typography} from "@material-ui/core";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import {BACKEND_URL} from "../config";
 
 // not working: import SmilesDrawer from "smiles-drawer";
 const SmilesDrawer = require("smiles-drawer");
@@ -18,9 +20,21 @@ const useStyles = makeStyles((theme) => ({
   button: {
       marginLeft: "16px"
   },
-  canvas: {
-      width: "500px",
-      height: "500px"
+  heading: {
+      margin: "2em 0 1em 0"
+  },
+  table: {
+      wordBreak: "break-word"
+  },
+  trLeft: {
+      width: "125px",
+      paddingLeft: "0"
+  },
+  trRight: {
+      paddingRight: "0"
+  },
+  noBorder: {
+      border: "none"
   }
 }));
 
@@ -35,17 +49,47 @@ function Home() {
 
     const { token } = useParams();
 
-    const [result, setResult] = useState<string>( "" );
+    const [chemicals,             setChemicals            ] = useState( [] );
+    const [input,                 setInput                ] = useState<string>( "" );
+    const [showInfos,             setShowInfos            ] = useState<boolean>( false );
+    const [chemicalInfo,          setChemicalInfo         ] = useState<any>( {} );
+    const [chemicalInfoIsLoading, setChemicalInfoIsLoading] = useState<boolean>( false );
+    const [result,                setResult               ] = useState<string>( "" );
+    const [resultIsLoading,       setResultIsLoading      ] = useState<boolean>( false );
 
 
-    const handleOnChange = (e:any) => {
+    useEffect(() => {  // equivalent to componentDidMount
 
-        let options = {};
-        let smilesDrawer = new SmilesDrawer.Drawer( options );
+        fetchChemicals();
+    }, []);
 
-        SmilesDrawer.parse(e.target.value, function(tree:any) {
+
+    useEffect(() => {  // equivalent to componentDidUpdate
+
+        // the smiles cannot be drawn in calculate because the canvas
+        // is not rendered at that time
+
+        const smilesDrawer = new SmilesDrawer.Drawer({ height: 300, width: 500 });
+
+        SmilesDrawer.parse(input, function(tree:any) {
             smilesDrawer.draw(tree, "smiles-drawer", "light", false);
         });
+    }, [showInfos]);
+
+
+    const fetchChemicals = async () => {
+
+        const response = await fetch( BACKEND_URL + "/chemicals" );
+
+        setChemicals( await response.json() );
+    }
+
+
+    const handleOnInputChange = (value:string) => {
+
+        setShowInfos( false );
+
+        setInput( value );
 	};
 
 
@@ -57,19 +101,104 @@ function Home() {
 
     const calculate = () => {
 
+        setShowInfos( true );
+
+        fetchChemicalInfos();
+        fetchResult();
+    }
+
+
+    const fetchChemicalInfos = async () => {
+
+        // TODO: error handling
+        
+        setChemicalInfoIsLoading( true );
+
+        const response = await fetch( `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastidentity/smiles/${input}/JSON` );
+
+        const json = await response.json();
+
+        const properties = json.PC_Compounds[0].props;
+
+        const chemicalInfo:any = {};
+
+        properties.forEach((child:any) => {
+            if( child.urn.label === "IUPAC Name" && child.urn.name === "Preferred" )
+                chemicalInfo.IUPACName = child.value.sval;
+            else if( child.urn.label === "Molecular Formula" )
+                chemicalInfo.MolecularFormula = child.value.sval;
+            else if( child.urn.label === "Molecular Weight" )
+                chemicalInfo.MolecularWeight = child.value.fval;
+            else if( child.urn.label === "InChI" )
+                chemicalInfo.InChI = child.value.sval;
+            else if( child.urn.label === "InChIKey" )
+                chemicalInfo.InChIKey = child.value.sval;
+        });
+
+        setChemicalInfoIsLoading( false );
+
+        setChemicalInfo( chemicalInfo );
+    }
+
+
+    const fetchResult = async () => {
+
+        setResultIsLoading( true );
+
         // TODO: implement
+        // const response = await fetch( BACKEND_URL + "/chemicals/" + input, { method: "POST" });
+        // const json = await response.json();
+
+        setResultIsLoading( false );
+        setResult( "ANTWORT VOM SERVER" );
     }
 
 
     return (
         <Paper className="paper">
 		    <div className={classes.row}>
-		        <TextField placeholder="SMILES" variant="outlined" className={classes.textField} onChange={handleOnChange} />
+		        <Autocomplete freeSolo options={chemicals} className={classes.textField} onChange={(e:any, value:any) => { if(value != null) handleOnInputChange(value.smiles); }} getOptionLabel={(option:any) => option.smiles} renderInput={(params) => <TextField {...params} placeholder="SMILES" variant="outlined" onChange={(e:any) => handleOnInputChange(e. target.value)} />} />
 		        <Button variant="contained" color="primary" className={classes.button} onClick={showLastCalculation}><SvgIcon>{KeyboardArrowDownIcon}</SvgIcon></Button>
 		        <Button variant="contained" color="primary" className={classes.button} onClick={calculate}><SvgIcon>{SearchIcon}</SvgIcon></Button>
 		    </div>
-            <canvas id="smiles-drawer" className={classes.canvas} />
-            {result && (<p>{result}</p>)}
+            {showInfos &&
+            <div>
+                <Typography variant="h6" className={classes.heading}>Struktur</Typography>
+                <canvas id="smiles-drawer" />
+
+                <Typography variant="h6" className={classes.heading}>Identifizierer und Eigenschaften</Typography>
+                {chemicalInfoIsLoading && <CircularProgress />}
+                {!chemicalInfoIsLoading &&
+                    <Table className={classes.table}>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className={classes.trLeft}>IUPAC Name</TableCell>
+                                <TableCell className={classes.trRight}>{chemicalInfo.IUPACName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={classes.trLeft}>Chemische Formel</TableCell>
+                                <TableCell className={classes.trRight}>{chemicalInfo.MolecularFormula}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={classes.trLeft}>Molekülmasse</TableCell>
+                                <TableCell className={classes.trRight}>{chemicalInfo.MolecularWeight} g/mol</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={classes.trLeft}>InChI</TableCell>
+                                <TableCell className={classes.trRight}>{chemicalInfo.InChI}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={`${classes.trLeft} ${classes.noBorder}`}>InChIKey</TableCell>
+                                <TableCell className={`${classes.trRight} ${classes.noBorder}`}>{chemicalInfo.InChIKey}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>}
+
+                <Typography variant="h6" className={classes.heading}>Ergebnis</Typography>
+                {resultIsLoading && <CircularProgress />}
+                {!resultIsLoading && <span>{result}</span>}
+            </div>
+            }
 		</Paper>
 	);
 }
