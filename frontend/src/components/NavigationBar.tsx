12b0e1fcacaf810fefa6060f5005267cbcdcfe6d
Matthias Nunn <matthiasnunn@gmx.de>
2020-05-23 02:01:39 +0200

    add logout prototype

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,20 +1,26 @@
-import {AppBar, makeStyles, Theme, Toolbar, Typography, Button} from '@material-ui/core';
+import {AppBar, IconButton, makeStyles, Theme, Toolbar, Typography} from '@material-ui/core';
 import React from 'react';
+import {matchPath} from 'react-router';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import WarningIcon from '@material-ui/icons/Warning';
 
 
 const useStyles = makeStyles((theme: Theme) => ({
 
-	title: {
-		color: 'inherit',
-		textTransform: 'none',
+	toolbar: {
+		display: 'flex',
+		justifyContent: 'space-between'
 	},
-	button: {
-		color: 'inherit',
+	brand: {
+		display: 'flex',
+		alignItems: 'center'
 	},
 	logo: {
-		marginRight: theme.spacing(2),
+		marginRight: theme.spacing(2)
 	},
+	iconButton: {
+		color: 'white'
+	}
 }));
 
 
@@ -22,11 +28,33 @@ function NavigationBar() {
 
 	const classes = useStyles();
 
+
+	const logout = () => {
+
+		// token must be parse manually from url, because useParams is not working
+
+		let url:string = window.location.href;
+
+		let token = url.match( /([^\/]+$)/g );
+
+		// TODO: logout
+
+		// if( token )
+			// token[0]
+	}
+
+
 	return (
         <AppBar position="sticky" color="primary">
-            <Toolbar>
-                <WarningIcon className={classes.logo} />
-                <Typography variant="h6">Toxic Webapp</Typography>
+            <Toolbar className={classes.toolbar}>
+				<div className={classes.brand}>
+                	<WarningIcon className={classes.logo} />
+                	<Typography variant="h6">Toxic Webapp</Typography>
+				</div>
+				{matchPath(window.location.pathname, {
+					path: '/home/:token',
+					exact: true
+				}) ? <IconButton className={classes.iconButton} onClick={logout}><ExitToAppIcon /></IconButton> : ""}
             </Toolbar>
         </AppBar>
 	);
